f00c8f1c1c04f09d80b89bfd3026df45b1dbce0a
janadrawert <jd090@hdm-stuttgart.de>
2022-04-14 14:05:50 +0200

    Update BOs (Erste Hälfte)

--- backend/server/bo/Aktivitaet.py
+++ backend/server/bo/Aktivitaet.py
@@ -4,6 +4,7 @@ class Aktivitaet(bo):
     # Die Klasse BusinessObjekt dient als Basisklasse für alle Objekte in der Klasse Akitivitaet
     def __init__(self):
         # Definieren der Attribute, der Klasse Aktivitaet
+        super().__init__()
         self._bezeichnung = ''
         self._kapazitaet = 0.0
         
@@ -23,3 +24,14 @@ class Aktivitaet(bo):
         # Setzen der Kapazitaet
         self._kapazitaet = kapazitaet
     
+    def __str__(self):
+        #Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz
+        return f'Aktivitaet: {self.get_bezeichnung()}, {self.get_kapazitaet()}'
+    
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        #Umwandeln eines Python dict() in eine Aktivitaet().
+        obj = Aktivitaet()
+        obj.set_bezeichnung(dictionary["bezeichnung"])
+        obj.set_kapazitaet(dictionary["kapazitaet"])
+        return obj
--- backend/server/bo/Buchung.py
+++ backend/server/bo/Buchung.py
@@ -4,6 +4,7 @@ class Buchung(bo):
     # Die Klasse BusinessObjekt dient als Basisklasse für alle Objekte in der Klasse Buchung
     def __init__(self):
         # Definieren der Attribute, der Klasse Buchung
+        super().__init__()
         self._erstellt_von = ''
         
     def get_erstellt_von(self):
@@ -13,4 +14,14 @@ class Buchung(bo):
     def set_erstellt_von(self, erstellt_von):
         # Setzen von erstellt_von
         self._erstellt_von = erstellt_von
-        
+    
+    def __str__(self):
+        #Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz
+        return f'Buchung: {self.get_erstellt_von()}'
+    
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        #Umwandeln eines Python dict() in eine Buchung().
+        obj = Buchung()
+        obj.set_erstellt_von(dictionary["erstellt_von"])
+        return obj
