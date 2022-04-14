e610fcc98eabe952d24396add8663b67b89070e1
Perry Dettke <perry.dettke@gmail.com>
2022-04-14 16:53:19 +0200

    Create Mapper/Datenbank connection

---
+++ backend/server/db/Mapper.py
@@ -0,0 +1,63 @@
+import mysql.connector as connector
+import os
+from contextlib import AbstractContextManager
+from abc import ABC, abstractmethod
+
+
+class Mapper (AbstractContextManager, ABC):
+    """Abstrakte Basisklasse aller Mapper-Klassen"""
+    
+    def __init__(self):
+        self._cnx = None
+
+    def __enter__(self):
+        
+        if os.getenv('GAE_ENV', '').startswith('standard'):
+            """Landen wir in diesem Zweig, so haben wir festgestellt, dass der Code in der Cloud abläuft.
+            Die App befindet sich somit im **Production Mode** und zwar im *Standard Environment*.
+            Hierbei handelt es sich also um die Verbindung zwischen Google App Engine und Cloud SQL."""
+
+            self._cnx = connector.connect(user='root', password='password!',
+                                          #unix_socket='/cloudsql/timetracker-339621:europe-west3:timetracker-db',
+                                          database='timetracker')
+        else:
+            """Wenn wir hier ankommen, dann handelt sich offenbar um die Ausführung des Codes in einer lokalen Umgebung,
+            also auf einem Local Development Server. Hierbei stellen wir eine einfache Verbindung zu einer lokal
+            installierten mySQL-Datenbank her."""
+
+            self._cnx = connector.connect(user='root', password='password!',
+                                  host='127.0.0.1',
+                                  database='timetracker')
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Was soll geschehen, wenn wir (evtl. vorübergehend) aufhören, mit dem Mapper zu arbeiten?"""
+        self._cnx.close()
+
+    """Formuliere nachfolgend sämtliche Auflagen, die instanzierbare Mapper-Subklassen mind. erfüllen müssen."""
+
+    @abstractmethod
+    def find_all(self):
+        """Lies alle Tupel aus und gib sie als Objekte zurück."""
+        pass
+
+    @abstractmethod
+    def find_by_key(self, id):
+        """Lies den einen Tupel mit der gegebenen ID (vgl. Primärschlüssel) aus."""
+        pass
+
+    @abstractmethod
+    def insert(self, object):
+        """Füge das folgende Objekt als Datensatz in die DB ein."""
+        pass
+
+    @abstractmethod
+    def update(self, object):
+        """Ein Objekt auf einen bereits in der DB enthaltenen Datensatz abbilden."""
+        pass
+
+    @abstractmethod
+    def delete(self, object):
+        """Den Datensatz, der das gegebene Objekt in der DB repräsentiert löschen."""
+        pass
