8ba918fba6c75c95396eb8f8b2ac435f40991c75
Raphael-Mueller <73542589+Raphael-Mueller@users.noreply.github.com>
2022-04-15 21:55:14 +0200

    ArbeitszeitkontoMapper: key funktion definiert

--- backend/server/db/ArbeitszeitkontoMapper.py
+++ backend/server/db/ArbeitszeitkontoMapper.py
@@ -61,3 +61,37 @@ class ArbeitszeitkontoMapper (Mapper):
         cursor.close()
 
         return result
+
+    def find_by_key(self, key):
+        """Suchen eines Benutzers mit vorgegebener Arbeitszeitkonto ID. Da diese eindeutig ist,
+        wird genau ein Objekt zurückgegeben.
+
+        :param key Primärschlüsselattribut (->DB)
+        :return Personen-Objekt, das dem übergebenen Schlüssel entspricht, None bei
+            nicht vorhandenem DB-Tupel.
+        """
+
+        result = []
+
+        cursor = self._cnx.cursor()
+        command = "SELECT id, letzte_aenderung, arbeitsleistung,  buchung_id FROM person WHERE id={}".format(key)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (id, letzte_aenderung, arbeitsleistung, buchung_id) = tuples[0]
+            arbeitszeitkonto = Arbeitszeitkonto()
+            arbeitszeitkonto.set_id(id)
+            arbeitszeitkonto.set_letzte_aenderung(letzte_aenderung)
+            arbeitszeitkonto.set_arbeitsleistung(arbeitsleistung)
+            arbeitszeitkonto.set_buchung_id(buchung_id)
+            result.append(arbeitszeitkonto)
+        except IndexError:
+            """Der IndexError wird oben beim Zugriff auf tuples[0] auftreten, wenn der vorherige SELECT-Aufruf
+            keine Tupel liefert, sondern tuples = cursor.fetchall() eine leere Sequenz zurück gibt."""
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
