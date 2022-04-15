c3b455555555833bcdff4a2f46c3a4c7abfc19e8
Raphael-Mueller <73542589+Raphael-Mueller@users.noreply.github.com>
2022-04-15 21:35:09 +0200

    ArbeitszeitkontoMapper: buchung_id funktion definiert

--- backend/server/db/ArbeitszeitkontoMapper.py
+++ backend/server/db/ArbeitszeitkontoMapper.py
@@ -34,3 +34,30 @@ class ArbeitszeitkontoMapper (Mapper):
         cursor.close()
 
         return result
+
+    def find_by_buchung_id(self, buchung_id):
+        """Auslesen aller Buchungen eines durch Fremdschlüssel
+
+        :param buchung_id Schlüssel der zugehörigen buchung.
+        :return Eine Sammlung mit Buchung-Objekten.
+        """
+
+        result = []
+        cursor = self._cnx.cursor()
+        command = "SELECT id, letzte_aenderung, arbeitsleistung FROM arbeitszeitkonto WHERE buchung_id={}".format(
+            buchung_id)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        for (id, letzte_aenderung, arbeitsleistung, buchung_id ) in tuples:
+            arbeitszeitkonto = Arbeitszeitkonto()
+            arbeitszeitkonto.set_id(id)
+            arbeitszeitkonto.set_letzte_aenderung(letzte_aenderung)
+            arbeitszeitkonto.set_arbeitsleistung(arbeitsleistung)
+            arbeitszeitkonto.set_buchung_id(buchung_id)
+            result.append(arbeitszeitkonto)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
