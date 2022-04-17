9258bb32565160a64b139e7ae25eaccdf03c77ab
KaiSchmied <kaischmied@icloud.com>
2022-04-17 15:03:04 +0200

    PersonBO fertig gecodet

--- reactfrontend/src/api/PersonBO.js
+++ reactfrontend/src/api/PersonBO.js
@@ -12,23 +12,49 @@ export default class PersonBO extends BusinessObject {
         this.arbeitszeitkonto_id = arbeitszeitkonto_id;
     }
 
-setVor_name(vor_name) {
+    setVor_name(vor_name) {
     this.vor_name = vor_name;
-}
+    }
 
-getVor_name(vor_name) {
-    return this.vor_name 
-}
+    getVor_name() {
+    return this.vor_name;
+    }
 
-setNach_name(nach_name) {
+    setNach_name(nach_name) {
     this.Nnach_name = nach_name;
-}
+    }  
 
-getNach_name(nach_name) {
-    return this.Nnach_name
-}
+    getNach_name() {
+    return this.Nnach_name;
+    }
 
+    setEmail(email) {
+    this.email = email;
+    }
 
+        getEmail() {
+    return this.email;
+    }
 
+    setBenutzer_name(benutzer_name) {
+    this.benutzer_name = benutzer_name;
+    }
 
+    getBenutzer_name() {
+    return this.benutzer_name;
+    }
+
+    setArbeitszeitkonto_id(arbeitszeitkonto_id) {
+    this.arbeitszeitkonto_id = arbeitszeitkonto_id;
+    }
+
+    getArbeitszeitkonto_id() {
+    return this.arbeitszeitkonto_id;
+    }
+
+    static fromJSON(person) {
+    // Objekt anhand einer JSON-Struktur erstellen
+    let p = Object.setPrototypeOf(person, PersonBO.prototype);
+    return p;
+    }
 }
