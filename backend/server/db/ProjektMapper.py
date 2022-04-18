93b05b5859de255c832533391e879a1bacf1bccb
Perry Dettke <perry.dettke@gmail.com>
2022-04-18 17:29:41 +0200

    ProjektMapper insert/update/delete

--- backend/server/db/ProjektMapper.py
+++ backend/server/db/ProjektMapper.py
@@ -2,7 +2,7 @@ from server.bo.Projekt import Projekt
 from server.db.Mapper import Mapper
 
 
-class ProjektnMapper (Mapper):
+class ProjektMapper (Mapper):
     """Mapper-Klasse, die Projekt-Objekte auf eine relationale
     Datenbank abbildet. Hierzu wird eine Reihe von Methoden zur Verfügung
     gestellt, mit deren Hilfe z.B. Objekte gesucht, erzeugt, modifiziert und
@@ -67,3 +67,80 @@ class ProjektnMapper (Mapper):
         cursor.close()
 
         return result
+
+    def insert(self, projekt):
+        """Einfügen eines Projekt-Objekts in die Datenbank.
+
+        Dabei wird auch der Primärschlüssel des übergebenen Objekts geprüft und ggf.
+        berichtigt.
+
+        :param projekt das zu speichernde Objekt
+        :return das bereits übergebene Objekt, jedoch mit ggf. korrigierter ID.
+        """
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT MAX(id) AS maxid FROM Projekt ")
+        tuples = cursor.fetchall()
+
+        for (maxid) in tuples:
+            if maxid[0] is not None:
+                """Wenn wir eine maximale ID festellen konnten, zählen wir diese
+                um 1 hoch und weisen diesen Wert als ID dem Projekt-Objekt zu."""
+                projekt.set_id(maxid[0] + 1)
+            else:
+                """Wenn wir KEINE maximale ID feststellen konnten, dann gehen wir
+                davon aus, dass die Tabelle leer ist und wir mit der ID 1 beginnen können."""
+                projekt.set_id(1)
+
+        command = "INSERT INTO projekt (id, letzte_aenderung, auftraggeber, aktivitaets_id) VALUES (%s,%s,%s,%s,)"
+        data = (
+            projekt.get_id(),
+            projekt.get_letzte_aenderung,
+            projekt.get_auftraggeber,
+            projekt.get_aktivitaets_id())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return projekt
+
+    def update(self, projekt):
+        """Wiederholtes Schreiben eines Objekts in die Datenbank.
+
+        :param projekt das Objekt, das in die DB geschrieben werden soll
+        """
+        cursor = self._cnx.cursor()
+
+        command = "UPDATE projekt " + "SET auftraggeber=%s, aktivitaets_id=%s WHERE id=%s"
+        data = (
+            projekt.get_auftraggeber(),
+            projekt.get_aktivitaets_id(),
+            projekt.get_id())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+
+    def delete(self, projekt):
+        """Löschen der Daten eines Projekt-Objekts aus der Datenbank.
+
+        :param projekt das aus der DB zu löschende "Objekt"
+        """
+        cursor = self._cnx.cursor()
+
+        command = "DELETE FROM projekt WHERE id={}".format(projekt.get_id())
+        cursor.execute(command)
+
+        self._cnx.commit()
+        cursor.close()
+
+
+"""Zu Testzwecken können wir diese Datei bei Bedarf auch ausführen, 
+um die grundsätzliche Funktion zu überprüfen.
+
+Anmerkung: Nicht professionell aber hilfreich..."""
+if (__name__ == "__main__"):
+    with ProjektMapper() as mapper:
+        result = mapper.find_all()
+        for projekt in result:
+            print(projekt)
