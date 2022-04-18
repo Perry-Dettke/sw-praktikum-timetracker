5286dc3a59c6b6bc8fb6d04781eb8fefd0a08c55
Perry Dettke <perry.dettke@gmail.com>
2022-04-18 16:45:23 +0200

    ProjektMapper find_all()

--- backend/server/db/ProjektMapper.py
+++ backend/server/db/ProjektMapper.py
@@ -12,3 +12,27 @@ class ProjektnMapper (Mapper):
 
     def __init__(self):
         super().__init__()
+        
+    def find_all(self):
+        """Auslesen aller Projekte unseres Systems.
+
+        :return Eine Sammlung mit Projekt-Objekten.
+        """
+        result = []
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT * from projekt")
+        tuples = cursor.fetchall()
+
+        for (id, letzte_aenderung, bezeichnung, auftraggeber, aktivitaet_id) in tuples:
+            projekt = Projekt()
+            projekt.set_id(id)
+            projekt.set_letzte_aenderung(letzte_aenderung)
+            projekt.bezeichnung(bezeichnung)
+            projekt.auftraggeber(auftraggeber)
+            projekt.aktivitaet_id(aktivitaet_id)
+            result.append(projekt)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
