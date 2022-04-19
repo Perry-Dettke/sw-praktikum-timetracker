ea345de08b7c446651c1e7150e37e07524384d86
jacky963 <jacqueline.schmid@gmx.de>
2022-04-19 10:46:51 +0200

    Create index.js

---
+++ reactfrontend/src/api/index.js
@@ -0,0 +1,11 @@
+/**
+ * Hier wird die komplette API als geschlossenes Modul exportieren
+ */
+ export {default as TimetrackerAPI} from './TimertrackerAPI';
+ export {default as ArbeitszeitkontoBO} from './ArbeitszeitkontoBO';
+ export {default as BuchungBO} from './BuchungBO';
+ export {default as EreignisBO} from './EreignisBO';
+ export {default as PersonBO} from './PersonBO';
+ export {default as PersonProjektBO} from './PersonProjektBO';
+ export {default as ProjektBO} from './ProjektBO';
+ export {default as ZeitintervallBO} from './ZeitintervallBO';
