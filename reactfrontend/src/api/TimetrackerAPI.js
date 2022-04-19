d22c941a3ba656bf06623795c3531c3b8d91b000
jacky963 <jacqueline.schmid@gmx.de>
2022-04-19 10:51:51 +0200

    Create TimetrackerAPI

---
+++ reactfrontend/src/api/TimetrackerAPI.js
@@ -0,0 +1,11 @@
+import ArbeitszeitkontoBO from './ArbeitszeitkontoBO';
+import BuchungBO from './BuchungBO';
+import EreignisBO from './EreignisBO';
+import PersomBO from './PersonBO';
+import PersonProjektBO from './PersonProjektBO';
+import ProjektBO from './ProjektBO';
+import ZeitintervallBO from './ZeitintervallBO';
+
+export default class TeachingbeeAPI {
+
+}
