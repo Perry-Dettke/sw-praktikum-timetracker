92994962c4ffe75811a9da77926ecb805eba0cf2
jacky963 <jacqueline.schmid@gmx.de>
2022-04-19 12:04:00 +0200

    Update Index.js

--- reactfrontend/src/api/index.js
+++ reactfrontend/src/api/index.js
@@ -2,6 +2,7 @@
  * Hier wird die komplette API als geschlossenes Modul exportieren
  */
  export {default as TimetrackerAPI} from './TimertrackerAPI';
+ export {default as AktivitaetBO} from './AktivitaetBO';
  export {default as ArbeitszeitkontoBO} from './ArbeitszeitkontoBO';
  export {default as BuchungBO} from './BuchungBO';
  export {default as EreignisBO} from './EreignisBO';
