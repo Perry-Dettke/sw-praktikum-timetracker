014c1594fe0185ba6e6f9b939bd7ca6f6128aa78
Perry Dettke <perry.dettke@gmail.com>
2022-04-20 11:46:45 +0200

    Strukturen der BusinessObjekte

--- backend/main.py
+++ backend/main.py
@@ -29,3 +29,73 @@ api = Api(app, version='1.0', title='Timetracker API',
 """Namespaces"""
 timetracker = api.namespace('app', description="Funktionen der App")
 
+
+"""Nachfolgend werden analog zu den BusinessObject-Klassen transferierbare Strukturen angelegt."""
+
+"""BusinessObject dient als Basisklasse."""
+
+bo = api.model('BusinessObject', {
+    'id': fields.Integer(attribute='_id',
+                         description='Der Unique Identifier eines Business Object'),
+
+    'last_change': fields.String(attribute='_person',                                          #hier eventuell DateTime
+                                description='Die Person die am BO die letzte Änderung durchgeführt hat'),
+    
+})
+
+aktivitaet = api.inherit('Aktivitaet', bo, {
+    'bezeichnung': fields.String(attribute='_bezeichnung',
+                                description='Bezeichnung einer Aktivitaet'),
+    'kapazitaet': fields.Integer(attribute='_kapazitaet',                               # Hier eventuell float?
+                                description='Kapazitaet einer Aktivitaet in Stunden'),
+})
+
+arbeitszeikonto = api.inherit('Arbeitszeitkonto', bo, {
+    'arbeitsleistung': fields.String(attribute='_arbeitsleistung',
+                                description='Arbeitsleistung im Arbeitszeitkonto'),
+    'buchung_id': fields.Integer(attribute='_buchung_id',
+                                description='ID einer Buchung einem Arbeitskonto zugeteilt'),
+})
+
+buchung = api.inherit('Buchung', bo, {
+    'person_id': fields.Integer(attribute='_person_id',
+                                description='ID der Person die die Buchung durchgeführt hat'),
+    'arbeitskonto_id': fields.Integer(attribute='_arbeitskonto_id',
+                                description='ID des Arbeitskonto auf dem die Buchung durchgeführt wird'),
+})
+
+person = api.inherit('Person', bo, {
+    'vor_name': fields.String(attribute='_nach_name',
+                                description='Vorname einer Person'),
+    'nach_name': fields.String(attribute='_nach_name',
+                                description='Nachname einer Person'),
+    'email': fields.String(attribute='_email',
+                                description='Email einer Person'),
+    'benutzername': fields.String(attribute='_benutzer_name',
+                                description='Benutzername einer Person'),
+    'arbeitszeitkonto_id': fields.Integer(attribute='_arbeitszeitkonto_id',
+                                description='ID des Arbeitszeitkonto einer Person'),
+    'projekt_id': fields.Integer(attribute='_projekt_id',
+                                description='ID eines Projekts an dem die Person arbeitet'),
+})
+
+projekt = api.inherit('Projekt', bo, {
+    'bezeichnung': fields.String(attribute='_bezeichnung',
+                                description='Bezeichnung eines Projekts'),
+    'auftraggeber': fields.String(attribute='_auftraggeber',
+                                description='Auftraggeber des Projekts'),
+    'aktivitaet_id': fields.String(attribute='_aktivitaet_id',
+                                description='IDs der Aktivitaeten im Projekt'),
+})
+
+zeitintervall = api.inherit('Zeitintervall', bo, {
+    'start': fields.String(attribute='_start',                              #String richtig?
+                            description='Start eines Zeitintervall'),
+    'ende': fields.String(attribute='_ende',                                #String richtig?
+                            description='Ende eines Zeitintervall'),
+})
+
+ereignis = api.inherit('Ereignis', bo, zeitintervall, {
+    'erstellungs_zeitpunkt': fields.DateTime(attribute='_erstellungs_zeitpunkt',       #DateTime richtig?
+                            description='Erstellungszeitpunkt eines Ereignis'),
+})
