df31d7212b7949e0856a8b207d3ac0884941953b
Rosalie Kripp <rk078@hdm-stuttgart.de>
2022-04-21 19:28:40 +0200

    BO Ereignis

---
+++ backend/server/bo/Ereignis.py
@@ -0,0 +1,29 @@
+import BusinessObjekt as bo
+
+class Ereignis(bo):
+    """ Die Klasse BusinessObjekt dient als Basisklasse für alle Objekte in der Klasse Ereignis. """
+    def __init__(self):
+        """ Definieren der Attribute, der Klasse Ereignis. """
+        super().__init__()
+        self._erstellungs_zeitpunkt = 0
+        
+    def get_erstellungs_zeitpunkt(self):
+        """ Ausgeben des Erstellungszeitpunkts. """
+        return self._erstellungs_zeitpunkt
+    
+    def set_erstellungs_zeitpunkt(self, erstellungs_zeitpunkt):
+        """ Setzen des Erstellungszeitpunkts. """
+        self._erstellungs_zeitpunkt = erstellungs_zeitpunkt
+    
+    def __str__(self):
+        """ Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz. """
+        return f'Ereignis: {self.get_erstellungs_zeitpunkt()}'
+    
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        """ Umwandeln eines Python dict() in ein Ereignis(). """
+        obj = Ereignis()
+        obj.set_erstellungs_zeitpunkt(dictionary["erstellungs_zeitpunkt"])
+        return obj
+
+# erstellt von Rosalie Kripp
