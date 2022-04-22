ea396cee67411402fc33f0eb64034862aac49419
jacky963 <jacqueline.schmid@gmx.de>
2022-04-22 10:59:27 +0200

    Update TimetrackerAPI.js

--- reactfrontend/src/api/TimetrackerAPI.js
+++ reactfrontend/src/api/TimetrackerAPI.js
@@ -1,11 +1,11 @@
-import AktivitaetBO from './AktivitaetBO';
-import ArbeitszeitkontoBO from './ArbeitszeitkontoBO';
-import BuchungBO from './BuchungBO';
-import EreignisBO from './EreignisBO';
-import PersomBO from './PersonBO';
-import PersonProjektBO from './PersonProjektBO';
-import ProjektBO from './ProjektBO';
-import ZeitintervallBO from './ZeitintervallBO';
+import AktivitaetBO from './AktivitaetBO'; //K 
+import ArbeitszeitkontoBO from './ArbeitszeitkontoBO'; //J
+import BuchungBO from './BuchungBO'; //J
+import EreignisBO from './EreignisBO'; //J 
+import PersonBO from './PersonBO'; //K
+import PersonProjektBO from './PersonProjektBO'; //K
+import ProjektBO from './ProjektBO'; //K
+import ZeitintervallBO from './ZeitintervallBO'; //J 
 
 export default class TeachingbeeAPI {
 
