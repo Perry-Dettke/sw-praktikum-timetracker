44cfb9e5020c16318fae2ee3d7483930ffaed834
KaiSchmied <kaischmied@icloud.com>
2022-04-22 11:02:05 +0200

    Anpassungen

--- reactfrontend/src/api/TimetrackerAPI.js
+++ reactfrontend/src/api/TimetrackerAPI.js
@@ -2,11 +2,14 @@ import AktivitaetBO from './AktivitaetBO';
 import ArbeitszeitkontoBO from './ArbeitszeitkontoBO';
 import BuchungBO from './BuchungBO';
 import EreignisBO from './EreignisBO';
-import PersomBO from './PersonBO';
+import PersonBO from './PersonBO';
 import PersonProjektBO from './PersonProjektBO';
 import ProjektBO from './ProjektBO';
 import ZeitintervallBO from './ZeitintervallBO';
 
 export default class TeachingbeeAPI {
 
+    static #api = null;
+
 }
+
