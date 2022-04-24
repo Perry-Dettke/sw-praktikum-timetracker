8c7a274ebbbd503f78d65ec0c42d867432c7ace5
Perry Dettke <perry.dettke@gmail.com>
2022-04-24 15:49:48 +0200

    Einrückfehler in BOs gefixt

--- backend/server/bo/Person.py
+++ backend/server/bo/Person.py
@@ -1,17 +1,17 @@
-import BusinessObjekt as bo
+from bo import BusinessObjekt
 
-class Person(bo):
+class Person(BusinessObjekt):
     """ Die Klasse BusinessObjekt dient als Basisklasse für alle Objekte in der Klasse Person. """
     def __init__(self):
         """ Definieren der Attribute, der Klasse Person. """
         super().__init__()
         self._vor_name = ""
-	    self._nach_name = ""
-	    self._email = ""
-	    self._benutzer_name = ""
-	    self._arbeitszeitkonto_id = 0
-	    self._projekt_id = 0
-        
+        self._nach_name = ""
+        self._email = ""
+        self._benutzer_name = ""
+        self._arbeitszeitkonto_id = 0
+        self._projekt_id = 0
+
     def get_vor_name(self):
         """ Ausgeben des Vornamens. """
         return self._vor_name
@@ -69,11 +69,11 @@ class Person(bo):
         """ Umwandeln eines Python dict() in eine Person(). """
         obj = Person()
         obj.set_vor_name(dictionary["vor_name"])
-	    obj.set_nach_name(dictionary["nach_name"])
-	    obj.set_email(dictionary["email"])
-	    obj.set_benutzer_name(dictionary["benutzer_name"])
-	    obj.set_arbeitszeitkonto_id(dictionary["arbeitszeitkonto_id"])
-	    obj.set_projekt_id(dictionary["projekt_id"])
+        obj.set_nach_name(dictionary["nach_name"])
+        obj.set_email(dictionary["email"])
+        obj.set_benutzer_name(dictionary["benutzer_name"])
+        obj.set_arbeitszeitkonto_id(dictionary["arbeitszeitkonto_id"])
+        obj.set_projekt_id(dictionary["projekt_id"])
         return obj
 
 # erstellt von Rosalie Kripp
