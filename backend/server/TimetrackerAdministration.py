95abb40bd25363f94cb2b646584a169e753c9297
Perry Dettke <perry.dettke@gmail.com>
2022-04-24 16:05:02 +0200

    Arbeitszeitkonto/Aktivitaet-spezifische Methoden

--- backend/server/TimetrackerAdministration.py
+++ backend/server/TimetrackerAdministration.py
@@ -1,3 +1,4 @@
+from re import A
 from .bo.Aktivitaet import Aktivitaet
 from .bo.Arbeitszeitkonto import Arbeitszeitkonto
 from .bo.Buchung import Buchung
@@ -73,3 +74,82 @@ class TimetrackerAdministration (object):
         """Die gegebenene Person aus unserem System löschen."""
         with PersonMapper() as mapper:
             mapper.delete(person)
+
+    """
+    Aktivitaet-spezifische Methoden
+    """
+    def create_aktivitaet(self, letzte_aenderung, bezeichnung, kapazitaet):
+        """Eine Aktivitaet anlegen"""
+        aktivitaet = Aktivitaet()
+        aktivitaet.set_letzte_aenderung(letzte_aenderung)
+        aktivitaet.set_bezeichnung(bezeichnung)
+        aktivitaet.set_bezeichnung(kapazitaet)
+        aktivitaet.set_id(1)
+
+        with AktivitaetMapper() as mapper:
+            return mapper.insert(aktivitaet)
+
+    def get_aktivitaet_by_id(self, key):
+        """Die Aktivitaet mit der gegebenen ID auslesen."""
+        with AktivitaetMapper() as mapper:
+            return mapper.find_by_key(key)
+
+    def get_aktivitaet_by_kapazitaet(self, kapazitaet):
+        """Die Aktivitaet mit der gegebenen kapazitaet auslesen."""
+        with AktivitaetMapper() as mapper:
+            return mapper.find_by_kapazitaet(kapazitaet)            # muss noch im Mapper geschrieben werden
+
+    def get_all_aktivitaet(self):
+        """Alle Aktivitaeten auslesen."""
+        with AktivitaetMapper() as mapper:
+            return mapper.find_all()
+
+    def save_aktivitaet(self, aktivitaet):
+        """Die gegebenen Aktivitaet speichern."""
+        with AktivitaetMapper() as mapper:
+            mapper.update(aktivitaet)
+
+    def delete_aktvitaet(self, aktivitaet):
+        """Die gegebenene Aktivitaet aus unserem System löschen."""
+        with AktivitaetMapper() as mapper:
+            mapper.delete(aktivitaet)
+
+
+    """
+    Arbeitszeitkonto-spezifische Methoden
+    """
+    def create_arbeitszeitkonto(self, letzte_aenderung, arbeitsleistung, buchung_id):
+        """Ein Arbeitszeitkonto anlegen"""
+        arbeitszeitkonto = Arbeitszeitkonto()
+        arbeitszeitkonto.set_letzte_aenderung(letzte_aenderung)
+        arbeitszeitkonto.set_arbeitsleistung(arbeitsleistung)
+        arbeitszeitkonto.set_buchung_id(buchung_id)
+        arbeitszeitkonto.set_id(1)
+
+        with ArbeitszeitkontoMapper() as mapper:
+            return mapper.insert(arbeitsleistung)
+
+    def get_arbeitszeitkonto_by_id(self, key):
+        """Das Arbeitszeitkonto mit der gegebenen ID auslesen."""
+        with ArbeitszeitkontoMapper() as mapper:
+            return mapper.find_by_key(key)
+
+    def get_arbeitszeitkonto_by_buchung_id(self, buchung_id):
+        """Das Arbeitszeitkonto mit der gegebenen Buchungs ID auslesen."""
+        with ArbeitszeitkontoMapper() as mapper:
+            return mapper.find_by_buchung_id(buchung_id)
+
+    def get_all_arbeitszeitkonto(self):
+        """Alle Arbeitszeitkonto auslesen."""
+        with Arbeitszeitkonto() as mapper:
+            return mapper.find_all()
+
+    def save_arbeitszeitkonto(self, arbeitszeitkonto):
+        """Das gegebenen Arbeitszeitkonto speichern."""
+        with ArbeitszeitkontoMapper() as mapper:
+            mapper.update(arbeitszeitkonto)
+
+    def delete_arbeitszeitkonto(self, arbeitszeitkonto):
+        """Das gegebenene Arbeitszeitkonto aus unserem System löschen."""
+        with Arbeitszeitkonto() as mapper:
+            mapper.delete(arbeitszeitkonto)
--- backend/server/bo/Arbeitszeitkonto.py
+++ backend/server/bo/Arbeitszeitkonto.py
@@ -6,7 +6,7 @@ class Arbeitszeitkonto(bo):
         # Definieren der Attribute, der Klasse Arbeitszeitkonto
         super().__init__()
         self._arbeitsleistung = 0.0
-        self._buchung = 0
+        self._buchung_id = 0
         
     def get_arbeitsleistung(self):
         # Ausgeben der Arbeitsleistung
@@ -16,22 +16,22 @@ class Arbeitszeitkonto(bo):
         # Setzen der Arbeitsleistung
         self._bezeichnung = arbeitsleistung
         
-    def get_buchung(self):
-        # Ausgeben der Buchung
-        return self._buchung
+    def get_buchung_id(self):
+        # Ausgeben der Buchungs ID
+        return self._buchung_id
     
-    def set_buchung(self, buchung):
-        # Setzen der Buchung
-        self._buchung = buchung
+    def set_buchung_id(self, buchung_id):
+        # Setzen der Buchungs ID
+        self._buchung_id = buchung_id
         
     def __str__(self):
         #Erzeugen einer einfachen textuellen Darstellung der jeweiligen Instanz
-        return f'Arbeitszeitkonto: {self.get_arbeitsleistung()}, {self.get_buchung()}'
+        return f'Arbeitszeitkonto: {self.get_arbeitsleistung()}, {self.get_buchung_id()}'
     
     @staticmethod
     def from_dict(dictionary=dict()):
         #Umwandeln eines Python dict() in eine Arbeitszeitkonto().
         obj = Arbeitszeitkonto()
         obj.set_arbeitsleistung(dictionary["arbeitsleistung"])
-        obj.set_buchung(dictionary["buchung"])
+        obj.set_buchung_id(dictionary["buchung_id"])
         return obj
