b6510fe8515fe06cc45220ed887942cfe8078882
janadrawert <jd090@hdm-stuttgart.de>
2022-05-02 17:49:50 +0200

    update imports

--- reactfrontend/src/components/layout/Header.js
+++ reactfrontend/src/components/layout/Header.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Component } from 'react'
 //import PropTypes from 'prop-types';
 import { Paper, Typography, Tabs, Tab } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
