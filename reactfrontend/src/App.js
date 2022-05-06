7c59fee476a5adb1bedc9d764d484324b53a8415
janadrawert <jd090@hdm-stuttgart.de>
2022-05-06 15:10:56 +0200

    Update App.js

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -2,17 +2,31 @@ import React from 'react';
 import './App.css';
 import Header from './components/layout/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <header className="App-header">
-        <p>
-         TEST TIMETRACKER APP
-        </p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+		super(props);
+
+		// Init an empty state
+		this.state = {
+			currentUser: null,
+			appError: null,
+			authError: null,
+			authLoading: false
+		};
+	}
+  
+  render(){
+    return (
+      <div className="App">
+        <Header />
+        <header className="App-header">
+          <p>
+          TEST TIMETRACKER APP
+          </p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
