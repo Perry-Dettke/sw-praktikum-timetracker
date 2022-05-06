13a43c339ea153731a4287ba3e46f406b97733ca
Perry Dettke <perry.dettke@gmail.com>
2022-05-06 15:34:25 +0200

    ProjektSeite erstellt

---
+++ reactfrontend/src/components/pages/ProjektSeite.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+class ProjektSeite extends React.Component {
+    render() {
+      return <h1>Hallo {this.props.name}</h1>;
+    }
+  }
+
+  export default ProjektSeite; 
