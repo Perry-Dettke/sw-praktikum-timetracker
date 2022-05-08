3b12b41df4a2f368bb0d7e3552940bc58f2edab9
jacky963 <jacqueline.schmid@gmx.de>
2022-05-08 11:00:56 +0200

    Update PersonSeite.js

---
+++ reactfrontend/src/components/pages/PersonSeite.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Component } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import {Button, Box} from '@mui/material';
+import Stack from '@mui/material/Stack';
+import PersonDialog from '../dialogs/PersonDialog';
