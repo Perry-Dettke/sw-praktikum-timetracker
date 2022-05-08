8599e1cfe88e0463f9504af9a2e6028713bc1414
KaiSchmied <kaischmied@icloud.com>
2022-05-08 11:25:32 +0200

    Person Page erstellt

--- reactfrontend/src/components/pages/PersonSeite.js
+++ reactfrontend/src/components/pages/PersonSeite.js
@@ -9,4 +9,34 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import {Button, Box} from '@mui/material';
 import Stack from '@mui/material/Stack';
+
 import PersonDialog from '../dialogs/PersonDialog';
+
+class Person extends Component {
+    
+    constructor(props) {
+        super(props);
+
+    this.state = {
+        showBuchung: false
+    };
+    }
+
+    //PersonDialog anzeigen
+    showBuchungDialog = () => {
+        this.setState({ showPerson: true}, () => {
+            console.log(this.state.showPerson);
+        });
+    };
+
+    //PersonDialog schließen
+    closeBuchungDialog = () => {
+        this.setState({ showPerson: false});
+    };
+
+    render() {
+
+    return (
+        <div></div>
+    )
+}}
