57fc50cb3de9b4df123d776be5ffd97aee10504f
Rosalie Kripp <rk078@hdm-stuttgart.de>
2022-05-09 17:07:38 +0200

    update BuchungDialog

--- reactfrontend/src/components/dialogs/BuchungDialog.js
+++ reactfrontend/src/components/dialogs/BuchungDialog.js
@@ -22,15 +22,55 @@ class BuchungDialog extends Component {
             show ?
                 <div>
                     <Dialog open={show} onClose={this.handleClose} maxWidth='xs'>
-                        <DialogTitle id='form-dialog-title'>Test
+                        <DialogTitle id='form-dialog-title'>Neue Buchung
                             <IconButton onClick={this.handleClose}>
                                 <CloseIcon />
                             </IconButton>
                         </DialogTitle>
                         <DialogContent>
-                            <DialogContentText>
-                                Buchung erstellen
-                            </DialogContentText>
+                            <div>
+                                {/* Projekt auswählen */}
+                                <FormControl sx={{ m: 0, minWidth: 1000 }}>
+                                    <InputLabel id="projekt">Projekt</InputLabel>
+                                    <Select
+                                    labelId="projekt"
+                                    name="projekt"
+                                    // value={this.state.projekt}
+                                    size="large"
+                                    label="projekt"
+                                    autoWidth
+                                    onChange={this.handleChange}
+                                    >
+                                    <MenuItem value={1}>Projekt 1</MenuItem>
+                                    <MenuItem value={2}>Projekt 2</MenuItem>
+                                    <MenuItem value={3}>Projekt 3</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </div>
+                            <div>
+                                {/* Aktivität auswählen */}
+                                <FormControl sx={{ m: 0, minWidth: 1000 }}>
+                                    <InputLabel id="aktivitaet">Aktivität</InputLabel>
+                                    <Select
+                                    labelId="aktivitaet"
+                                    name="aktivitaet"
+                                    // value={this.stateaktivitaet}
+                                    size="large"
+                                    label="aktivitaet"
+                                    autoWidth
+                                    onChange={this.handleChange}
+                                    >
+                                    <MenuItem value={1}>Aktivität 1</MenuItem>
+                                    <MenuItem value={2}>Aktivität 2</MenuItem>
+                                    <MenuItem value={3}>Aktivität 3</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </div>
+                            <div>
+                                <Button variant="outlined">Ereignis</Button>
+                                <Button variant="outlined">Zeitintervall</Button>
+                            </div>
+                            {/* Buttons für Ereignis & Zeitintervall fehlen jeweils */}
                         </DialogContent>
                         <DialogActions>
                             <Button color='secondary' onClick={this.handleClose}>
