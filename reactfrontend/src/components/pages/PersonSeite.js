bc302f2763749b09d538ed10e5af9c02123fa526
KaiSchmied <kaischmied@icloud.com>
2022-05-10 11:02:27 +0200

    Update PersonSeite.js

--- reactfrontend/src/components/pages/PersonSeite.js
+++ reactfrontend/src/components/pages/PersonSeite.js
@@ -7,8 +7,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import {Button, Box} from '@mui/material';
-import Stack from '@mui/material/Stack';
+import {Button} from '@mui/material';
+//import Stack from '@mui/material/Stack';
 import PersonDialog from '../dialogs/PersonDialog';
 
 class Person extends Component {
