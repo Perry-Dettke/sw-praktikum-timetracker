cdb1edaa24b5a4f6b696553c78f13674e82993ab
KaiSchmied <kaischmied@icloud.com>
2022-05-10 11:00:10 +0200

    Update PersonSeite.js

--- reactfrontend/src/components/pages/PersonSeite.js
+++ reactfrontend/src/components/pages/PersonSeite.js
@@ -9,7 +9,6 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import {Button, Box} from '@mui/material';
 import Stack from '@mui/material/Stack';
-
 import PersonDialog from '../dialogs/PersonDialog';
 
 class Person extends Component {
@@ -39,44 +38,39 @@ class Person extends Component {
 
         return (
             <div>
-                <Button variant="contained" sx={{width:250}}
-                        onClick={this.showPersonDialog}>
-                Neue Person Anlegen</Button>
-                <Box
-                sx={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                '& > :not(style)': {
-                    m: 2,
-                    width: 'max',
-                    height: 800,
-                    alignItems: 'center',
-                    },
-                }}
-                >
-                    <TableContainer component={Paper}  sx={{ maxWidth: 1200 , margin:"auto", }}>
-                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell align="left"><b>Person</b></TableCell>
-                                    <TableCell align="left"><b>löschen</b></TableCell>
-                                    <TableCell align="left"><b>bearbeiten</b></TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>                                
-                                <TableRow>
-                                  <TableCell align="left">Test</TableCell>
-                                    <TableCell align="left">Test</TableCell>
-                                    <TableCell align="left">Test</TableCell>
-                                </TableRow>
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
-                </Box>
+            <Button variant="contained" sx={{width:250}} onClick={this.showPersonDialog}> Neue Person Erstellen</Button>
+            <TableContainer component={Paper}  sx={{ maxWidth: 1000 , margin:"auto", }}>
+                <Table sx={{ minWidth: 600 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="left"><b>Person</b></TableCell>     
+                       </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell component="th" scope="row">Projekt 1</TableCell>
+                            <TableCell align="right"><Button variant="outlined">Delete</Button></TableCell>
+                            <TableCell align=""><Button variant="outlined">Edit</Button></TableCell>
+                       </TableRow>
+                        <TableRow>
+                            <TableCell component="th" scope="row">Projekt 2</TableCell>
+                            <TableCell align="right"><Button variant="outlined">Delete</Button></TableCell>
+                            <TableCell align=""><Button variant="outlined">Edit</Button></TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell component="th" scope="row">Projekt 3</TableCell>
+                            <TableCell align="right"><Button variant="outlined">Delete</Button></TableCell>
+                            <TableCell align=""><Button variant="outlined">Edit</Button></TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
             <PersonDialog show={showPerson} onClose={this.closePersonDialog}/>
              </div>
          );
     }
 }
+
+
 
 export default Person;
