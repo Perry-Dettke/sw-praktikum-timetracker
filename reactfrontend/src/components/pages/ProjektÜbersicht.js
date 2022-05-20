297636539880b0e631307c89ba63687a31906c69
janadrawert <jd090@hdm-stuttgart.de>
2022-05-20 23:40:41 +0200

    ProjektÜbersicht.js angelegt

---
+++ reactfrontend/src/components/pages/ProjektÜbersicht.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { Component } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+
+class Projektuebersicht extends Component {
+    
+    constructor(props) {
+        super(props);
+
+    this.state = {
+        showProjektuebersicht: false
+    };
+    }
+
+    render() {
+        const {showProjektuebersicht} = this.state;
+
+        return (
+            <div>
+            <TableContainer component={Paper}  sx={{ maxWidth: 50000, margin:"auto"}}>
+                <Table sx={{ minWidth: 600 }} aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell align="left"><b>Projekte</b></TableCell>
+                            <TableCell align="left"><b>Aktivität</b></TableCell>
+                            <TableCell align="left"><b>Soll Stunden</b></TableCell>
+                            <TableCell align="left"><b>Ist Stunden</b></TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell component="th" scope="row">Projekt 1</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell component="th" scope="row">Projekt 2</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell component="th" scope="row">Projekt 3</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            </div>
+        );
+    }
+}
+
+
+export default Projektuebersicht;
