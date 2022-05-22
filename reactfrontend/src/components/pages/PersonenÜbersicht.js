ccac45ed0586d3d9d1212b234ba1f195095679c5
Raphael-Mueller <73542589+Raphael-Mueller@users.noreply.github.com>
2022-05-22 22:46:48 +0200

    Tabelle für Personenübersicht erstellt

--- reactfrontend/src/components/pages/PersonenÜbersicht.js
+++ reactfrontend/src/components/pages/PersonenÜbersicht.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { Component } from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -8,66 +7,70 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import {Button} from '@mui/material';
-import PersonDialog from '../dialogs/PersonDialog';
-//import Stack from '@mui/material/Stack';
+import { Component } from 'react';
+//mport Stack from '@mui/material/Stack';
+import ProjektDialog from '../dialogs/ProjektDialog';
+import AktivitaetDialog from '../dialogs/AktivitaetDialog';
 
-
-class Personen_uebersicht extends Component {
+class personen_uebersicht extends Component {
     
     constructor(props) {
         super(props);
 
     this.state = {
-        showPerson: false
+        showProjekt: false
     };
+
+    this.state = {
+        showAktivitaet: false
+    };
+
     }
 
 
 
     render() {
-        const {showPerson} = this.state;
+    
 
         return (
             <div>
-            <TableContainer component={Paper}  sx={{ maxWidth: 1000 , margin:"auto", }}>
+
+            <TableContainer component={Paper}  sx={{ maxWidth: 1000, margin:"auto"}}>
                 <Table sx={{ minWidth: 600 }} aria-label="simple table">
+                    
                     <TableHead>
                         <TableRow>
-                            <TableCell align="left"><b>Person</b></TableCell>     
-                       </TableRow>
-                       <TableRow>
-                            <TableCell align="right"><b>Projekt</b></TableCell>     
-                       </TableRow>
-                       <TableRow>
-                            <TableCell align="left"><b>Soll Stunden</b></TableCell>     
-                       </TableRow>
-                       <TableRow>
-                            <TableCell align="left"><b>Ist Stunden</b></TableCell>     
-                       </TableRow>
+                            <TableCell align="left"><b>Person</b></TableCell>
+                            <TableCell align="left"><b>Projekt</b></TableCell>
+                            <TableCell align="left"><b>Soll Std</b></TableCell>
+                            <TableCell align="left"><b>Ist Std</b></TableCell>
+                        </TableRow>
                     </TableHead>
                     <TableBody>
                         <TableRow>
-                            <TableCell component="th" scope="row">Projekt 1</TableCell>
-                            <TableCell align="right"><Button variant="outlined">Delete</Button></TableCell>
-                            <TableCell align=""><Button variant="outlined">Edit</Button></TableCell>
-                       </TableRow>
-                        <TableRow>
-                            <TableCell component="th" scope="row">Projekt 2</TableCell>
-                            <TableCell align="right"><Button variant="outlined">Delete</Button></TableCell>
-                            <TableCell align=""><Button variant="outlined">Edit</Button></TableCell>
+                            <TableCell component="th" scope="row">Person</TableCell>
+                            
                         </TableRow>
                         <TableRow>
-                            <TableCell component="th" scope="row">Projekt 3</TableCell>
-                            <TableCell align="right"><Button variant="outlined">Delete</Button></TableCell>
-                            <TableCell align=""><Button variant="outlined">Edit</Button></TableCell>
+                            <TableCell component="th" scope="row">Person 2</TableCell>
+                        
+                        </TableRow>
+                        <TableRow>
+                            <TableCell component="th" scope="row">Person 3</TableCell>
+                            
                         </TableRow>
                     </TableBody>
                 </Table>
             </TableContainer>
-            <PersonDialog show={showPerson} onClose={this.closePersonDialog}/>
-             </div>
-         );
+         
+            
+            </div>
+        );
     }
 }
 
-export default Personen_uebersicht
+
+
+
+
+export default personen_uebersicht;
