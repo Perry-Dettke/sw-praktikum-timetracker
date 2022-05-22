df804e731c9bb8311e86103f2f9c2dd831f0f2eb
Perry Dettke <perry.dettke@gmail.com>
2022-05-22 23:30:02 +0200

    Conflict aufgelöst

--- reactfrontend/src/App.js
+++ reactfrontend/src/App.js
@@ -37,14 +37,9 @@ class App extends React.Component {
                 <Route path={'/home'} element={<Home/>} />
                 <Route path={'/projekt'} element={<ProjektListe/>} />
                 <Route path={'/person'} element={<PersonListe/>} />
-                <Route path={'/personen_uebersicht'} element={<Personen_uebersicht/>} />
-                {/*<Route path={'/aktivitaet'} element={<Aktivitaet/>} />*/}
                 <Route path={'/buchung'} element={<BuchungListe/>} />
-                {/*<Route path={'/projekt_uebersicht'} element={<Projektuebersicht/>} />*/}
-                {/*<Route path={'/personen_uebersicht'} element={<Personenuebersicht/>} />*/}
-
-                <Route path={'/buchung'} element={<Buchung/>} />
-                {/*<Route path={'/projekt_uebersicht'} element={<Projektuebersicht/>} />*/}         
+                <Route path={'/personen_uebersicht'} element={<Personen_uebersicht/>} />
+                {/*<Route path={'/projekt_uebersicht'} element={<Projektuebersicht/>} />*/}    
             </Routes> 
           </div>
         </div>
