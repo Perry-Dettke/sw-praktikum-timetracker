4c0c9d2aa318edeed6ba3dfe6d34bc80960c8d1e
janadrawert <jd090@hdm-stuttgart.de>
2022-05-25 20:30:37 +0200

    Update ProjektÜbersicht.js

--- reactfrontend/src/components/pages/ProjektÜbersicht.js
+++ reactfrontend/src/components/pages/ProjektÜbersicht.js
@@ -1,95 +1,102 @@
 import * as React from 'react';
 import { Component } from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+//import Table from '@mui/material/Table';
+//import TableBody from '@mui/material/TableBody';
+//import TableCell from '@mui/material/TableCell';
+//import TableContainer from '@mui/material/TableContainer';
+//import TableHead from '@mui/material/TableHead';
+//import TableRow from '@mui/material/TableRow';
+//import Paper from '@mui/material/Paper';
 //import List from '@mui/material/List';
 
-import TimetrackerAPI from '../../api/TimetrackerAPI';
+import { Button, TextField, InputAdornment, IconButton, Grid, Typography, Paper, List, Fab, Tooltip, Divider } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 
+
+import TimetrackerAPI from '../../api/TimetrackerAPI';
+import ProjektUebersichtEintrag from './ProjektUebersichtEintrag';
+import ProjektListenEintrag from './ProjektListenEintrag';
 
 class Projekt_uebersicht extends Component {
     
     constructor(props) {
         super(props);
 
-    this.state = {
-        projekt: null
+        //init empty state
+        this.state = {
+            projekt: [],
+            aktivitaet: [],
     };
     }
+
+    /** Fetches all PersonBOs from the backend */
+    getProjekt = () => {
+        var pro = TimetrackerAPI.getAPI();
+            pro.getProjekt().then((projektBOs) => {
+              this.setState({
+                projekt: projektBOs,
+              });
+            });
+    }
+    
+    getAktivitaetbyProjektID = () => {
+        var akt = TimetrackerAPI.getAPI();
+            akt.getAktivitaetbyProjektID().then((aktivitaetBOs) => {
+                this.setState({
+                    aktivitaet: aktivitaetBOs,
+                });
+            });
+    }
+
 
     componentDidMount() {
         this.getProjekt();
+        this.getAktivitaetbyProjektID();
     }
+    
 
-    getProjekt = () => {
-        TimetrackerAPI.getAPI().getProjekt().then((projektBOs) =>
-         //   console.log(projekt))
-        this.setState({
-                projekt: projektBOs,
-            })
-        ).catch((e) =>
-            this.setState({
-                projekt: null,
-            })
-        );
-    };
-
-    /*
-    getAktivitaetbyProjektID = () => {
-        TimetrackerAPI.getAPI().getAktivitaetbyProjektID().then((aktivitaetbyprojektid) =>
-            this.setState({
-                aktivitaetbyprojektid: aktivitaetbyprojektid,
-            })
-        ).catch((e) =>
-            this.setState({
-                aktivitaetbyprojektid: null,
-            })
-        );
-    };
-    */
-
+    /** Renders the component */
     render() {
-        const {projekt, aktivitaetbyprojektid} = this.state;
+        const { expandedState } = this.props;
+        
+        const{projekt, aktivitaet} = this.state;
+        console.log(projekt)
 
         return (
             <div>
-                <TableContainer component={Paper}  sx={{ maxWidth: 50000, margin:"auto"}}>
-                    <Table sx={{ minWidth: 600 }} aria-label="simple table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell align="left"><b>Projekte</b></TableCell>
-                                <TableCell align="left"><b>Aktivität</b></TableCell>
-                                <TableCell align="left"><b>Soll Stunden</b></TableCell>
-                                <TableCell align="left"><b>Ist Stunden</b></TableCell>
-                            </TableRow>
-                        </TableHead>
-                     
-                        <TableBody>
-                            <TableRow>
-                                <TableCell component="th" scope="row">Test</TableCell>
-                            </TableRow>
-                            {/* <TableRow>
-                                <TableCell component="th" scope="row">{aktivitaetbyprojektid.getAktivitaetbyProjektID()}</TableCell>
-                            </TableRow> */}
-                        </TableBody>
-                        {/*
-                        <TableBody>
-                            <TableRow>
-                                <TableCell component="th" scope="row">Keine Daten vorhanden</TableCell>
-                            </TableRow>
-                        </TableBody>
-                        */}
-                    </Table>
-                </TableContainer>
+                <Grid container spacing={4}  alignItems="left">
+                    <Grid item xs={12}>
+                        <Button 
+                            sx={{
+                                m: 4,
+                                width: 300,
+                                height: 50,
+                                alignItems: 'center',
+                                }}   variant="contained" color="primary" aria-label="add" onClick={this.addProjektButtonClicked}>
+                                <AddIcon />   
+                                neues Projekt anlegen
+                        </Button>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <List >
+                            {
+                                Object.values(projekt).map(projekt =>
+                                    <ProjektUebersichtEintrag key={Object.keys(projekt)[projekt.id]} projekt={projekt} aktivitaet={aktivitaet} show={this.props.show}
+                                        getProjekt={this.getProjekt} getAktivitaetbyProjektID={this.getAktivitaetbyProjektID} />)
+                            }
+
+                        
+                            
+                            Projekte Übersicht Eintrag hier anzeigen
+                         
+                        </List>
+                    </Grid>
+                </Grid>
             </div>
         );
     }
 }
+
 
 
 export default Projekt_uebersicht;
