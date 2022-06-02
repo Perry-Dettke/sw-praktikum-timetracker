d665b73f6ea473d3202927aec559dea3da1d7f1e
janadrawert <jd090@hdm-stuttgart.de>
2022-06-02 10:44:21 +0200

    Buchungsseite aktualisiert

--- reactfrontend/src/components/pages/BuchungListe.js
+++ reactfrontend/src/components/pages/BuchungListe.js
@@ -15,14 +15,12 @@ class BuchungListe extends Component {
 
     // Init an empty state
     this.state = {
-      buchung: null,
-      filteredBuchung: [],
-      showBuchungForm: false,
-      showBuchungDelete: false,
+      buchung: [],
+      showBuchungAnlegen: false,
     };
   }
-
-  /** Fetches all BuchungBOs from the backend */
+/*
+  // Fetches all BuchungBOs from the backend 
   getBuchung = () => {
     var api = TimetrackerAPI.getAPI();
         api.getBuchung().then((buchungBOs) => {
@@ -32,8 +30,6 @@ class BuchungListe extends Component {
         });
       }
 
-    // set loading to true
-
   // Add Button - Oeffnet den Buchung hinzufuegen Dialog
   addBuchungButtonClicked = event => {
     event.stopPropagation();
@@ -42,88 +38,60 @@ class BuchungListe extends Component {
     });
   }
 
+  //BuchungAnlegen anzeigen
+  showBuchungAnlegen = () => {
+      this.setState({ showBuchung: true}, () => {
+          // console.log(this.state.showBuchung);
+      });
+  };
+
+  //BuchungAnlegen schließen
+  closeBuchungDialog = () => {
+      this.setState({ showBuchung: false});
+  };
 
 
 
-
-
-
-
-//wird aufgerufen, wenn Dialog Fenster geschloßen wird
-buchungFormClosed = buchung => {
-  this.getBuchung();
-  if (buchung) {
-    const newBuchungList = [...this.state.buchung, buchung];
-    this.setState({
-      buchung: newBuchungList,
-      filteredBuchung: [...newBuchungList],
-      showBuchungForm: false
-    });
-  } else {
-    this.setState({
-      showBuchungForm: false
-    });
+  componentDidMount() {
+      this.getBuchung();
   }
-}
+  */
 
+  /** Renders the component */
+  render() {
 
+    const { buchung, showBuchungForm, showBuchungDelete } = this.state;
 
+    return (
+      <div>
+        {/* <Button variant="contained" sx={{width:250}} onClick={this.showBuchungDialog}> Neue Buchung Erstellen</Button> */}
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={12}>
+            <Button 
+                sx={{
+                    m: 1,
+                    width: 300,
+                    height: 50,
+                    alignItems: 'center',
+                    }}   variant="contained" color="primary" aria-label="add" onClick={this.projektAnlegenButtonClicked}>
+                    <AddIcon />   
+                    neue Buchung erstellen
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <List>
+              {
+                  buchung.map(buchung =>
+                      <BuchungListenEintrag key={buchung[buchung.id]} buchung={buchung} show={this.props.show}/>)
+                  }
+            </List>
+          </Grid>
+        </Grid>
+        {/* {<BuchungForm show={showBuchungForm} onClose={this.buchungFormClosed} getAtivitaet = {this.getBuchung}/>} */}
 
-
-
-    //BuchungDialog anzeigen
-    showBuchungDialog = () => {
-        this.setState({ showBuchung: true}, () => {
-            // console.log(this.state.showBuchung);
-        });
-    };
-
-    //BuchungDialog schließen
-    closeBuchungDialog = () => {
-        this.setState({ showBuchung: false});
-    };
-
-
-
-    componentDidMount() {
-        this.getBuchung();
-    }
-
-    /** Renders the component */
-    render() {
-
-        const { buchung, showBuchungForm, showBuchungDelete } = this.state;
-
-
-
-        return (
-            <div>
-                {/* <Button variant="contained" sx={{width:250}} onClick={this.showBuchungDialog}> Neue Buchung Erstellen</Button> */}
-                <Grid container spacing={2} alignItems="center">
-                </Grid>
-                <Grid item>
-                <Tooltip title='Buchung anlegen' placement="left">
-                    <Fab size="medium"  color="primary" aria-label="add" onClick={this.addBuchungButtonClicked}>
-                        <AddIcon />
-                    </Fab>
-                </Tooltip>
-            </Grid>
-                <Paper>
-                    <List >
-                        {
-                            Object.values(buchung).map(buchung =>
-                                <BuchungListenEintrag key={Object.keys(buchung)[buchung.id]} buchung={buchung} show={this.props.show}
-                                    getBuchung={this.getBuchung} />)
-
-
-                        }
-                    </List>
-                </Paper>
-                {/* {<BuchungForm show={showBuchungForm} onClose={this.buchungFormClosed} getAtivitaet = {this.getBuchung}/>} */}
-
-            </div>
-        );
-    }
+      </div>
+    );
+  }
 }
 
 
