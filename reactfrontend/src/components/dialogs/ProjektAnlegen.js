be46759e54fa77747334e481f2e6370f0a838791
Rosalie Kripp <rk078@hdm-stuttgart.de>
2022-06-14 22:39:18 +0200

    Multiselect Projektanlegen.js

--- reactfrontend/src/components/dialogs/ProjektAnlegen.js
+++ reactfrontend/src/components/dialogs/ProjektAnlegen.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Typography, TextField, OutlinedInput, Box, Chip } from '@mui/material';
+import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Typography, TextField, IconButton, OutlinedInput, Box, Chip } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import InputLabel from "@mui/material/InputLabel";
 
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import { MenuItem } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
 
 import TimetrackerAPI from '../../api/TimetrackerAPI';
+import ProjektBO from '../../api/ProjektBO';
 
 
 
@@ -21,9 +21,8 @@ class ProjektAnlegen extends Component {
         this.state = {
             allePersonen: [],
             personen: [],
-            personenCounter: 0,
-            aktivitaetenCounter: 0,
-            aktivitaeten: [],
+            projektBezeichnung: null,
+            auftraggeber: null,
         };
     }
 
@@ -36,103 +35,39 @@ class ProjektAnlegen extends Component {
         });
     }
 
-    // weitere Person hinzufügen
-    renderPersonenBranch = () => {
-        const { allePersonen } = this.state
-        return (
-            <>
-                <div>
-                    <FormControl fullWidth>
-                        <InputLabel id="person">Person</InputLabel>
-                        <Select
-                            labelId="Person"
-                            name="person"
-                            size="medium"
-                            label="Person"
-                            autoWidth
-                            onChange={this.handleChange}
-                        >
-
-                            {allePersonen.map((person) => (
-                                <MenuItem
-                                    key={person.getID()}
-                                    value={person.getVor_name()}
-                                >
-                                    {person.getVor_name()}
-                                </MenuItem>
-                            ))}
-                        </Select>
-                    </FormControl>
-                </div>
-                <br />
-            </>
-        )
-    }
-
-    renderPersonenBranches = () => {
-        const { personenCounter } = this.state
-        const result = []
-        for (let i = 0; i <= personenCounter; i++) {
-            result.push(this.renderPersonenBranch(i))
-        }
-        return result
-    }
-
-    appendPersonenDiv = () => {
-        this.setState({
-            personenCounter: this.state.personenCounter + 1,
-            personen: [
-                ...this.state.personen,
-            ]
+    // Projekt hinzufügen
+    addProjekt = () => {
+        let newProjekt = new ProjektBO()
+        newProjekt.setID(0)
+        newProjekt.setBezeichnung(this.state.projektBezeichnung)
+        newProjekt.setAuftraggeber(this.state.auftraggeber)
+        newProjekt.setProjekterstellerID(0)
+        //newProjekt.setProjekterstellerID(this.currentUser.getID())
+        TimetrackerAPI.getAPI().addProjekt(newProjekt).then(projekt => {
+            this.props.onClose(projekt); 
         })
     }
 
-    // weitere Aktivität hinzufügen
-    renderAktivitaetenBranch = () => {
-        return (
-            <>
-                <div>
-                    <TextField
-                        label="Aktivität"
-                        variant="outlined"
-                        name="name"
-                        size="small"
-                        autocomplete='off'
-                    />
-                    &emsp;
-                    <TextField
-                        label="Kapazität in Stunden"
-                        variant="outlined"
-                        multiline
-                        name="name"
-                        size="small"
-                        autocomplete='off'
-                    />
+    // Textfelder ändern
+    textFieldValueChange = (event) => {
+        const value = event.target.value;
 
-                </div>
-                <br />
-            </>
-        )
-    }
-
-    renderAktivitaetenBranches = () => {
-        const { aktivitaetenCounter } = this.state
-        const result = []
-        for (let i = 0; i <= aktivitaetenCounter; i++) {
-            result.push(this.renderAktivitaetenBranch(i))
+        let error = false;
+        if (value.trim().length === 0) {
+            error = true;
         }
-        return result
-    }
 
-    appendAktivitaetenDiv = () => {
         this.setState({
-            aktivitaetenCounter: this.state.aktivitaetenCounter + 1,
-            aktivitaeten: [
-                ...this.state.aktivitaeten,
-            ]
-        })
+            [event.target.id]: event.target.value,
+        });
     }
 
+    // Multiselect ändern
+    handleChange = (event) => {
+        this.setState({
+            personen: event.target.value,
+        });
+    }
 
     //Dialog schließen
     handleClose = () => {
@@ -145,8 +80,9 @@ class ProjektAnlegen extends Component {
     }
 
     render() {
-        const { show, projekt } = this.props
-        const { allePersonen, aktivitaeten } = this.state
+        const { show } = this.props
+        const { allePersonen, personen } = this.state
+        const {projektBezeichnung, auftraggeber} = this.state
 
         let title = 'Neues Projekt';
 
@@ -156,6 +92,9 @@ class ProjektAnlegen extends Component {
                     <Dialog open={show} onClose={this.handleClose} maxWidth='sm' fullWidth>
                         <DialogTitle>
                             {title}
+                            <IconButton onClick={this.handleClose}>
+                                <CloseIcon />
+                            </IconButton>
                         </DialogTitle>
                         <DialogContent>
                             <DialogContentText>
@@ -165,8 +104,8 @@ class ProjektAnlegen extends Component {
                                         variant="outlined"
                                         name="name"
                                         size="small"
-                                        // value={this.state.name}
-                                        onChange={this.handleChange}
+                                        value={projektBezeichnung}
+                                        onChange={this.textFieldValueChange}
                                         autocomplete='off'
                                     />
                                 </FormControl>
@@ -177,63 +116,55 @@ class ProjektAnlegen extends Component {
                                         variant="outlined"
                                         name="name"
                                         size="small"
-                                        // value={this.state.name}
-                                        onChange={this.handleChange}
+                                        value={auftraggeber}
+                                        onChange={this.textFieldValueChange}
                                         autocomplete='off'
                                     />
                                 </FormControl>
                                 <br /><br />
-                                {/* Personen die im System hinterlegt sind anzeigen lassen und button (o.ä.) einfügen um weitere Personen einfügen zu können*/}
+                                {/* Personen die im System hinterlegt sind anzeigen lassen und mehrere zum Projekt hinzufügen*/}
                                 {allePersonen ?
                                     <div>
-                                        {this.renderPersonenBranches()}
-                                        <Button
-                                            sx={{
-                                                marginLeft: 2,
-                                                marginTop: 0.5,
-                                                width: 500,
-                                                height: 30,
-                                                alignItems: 'center',
-                                            }}
-                                            variant="contained"
-                                            className="addbranch"
-                                            onClick={this.appendPersonenDiv}
-                                        >
-                                            <AddIcon />
-                                            &emsp;
-                                            <Typography>Weitere Person hinzufügen</Typography>
-                                        </Button>
+                                        <FormControl fullWidth>
+                                            <InputLabel id="person">Personen</InputLabel>
+                                            <Select
+                                                labelId="Person"
+                                                name="person"
+                                                multiple
+                                                size="medium"
+                                                label="Person"
+                                                value={personen}
+                                                autoWidth
+                                                onChange={this.handleChange}
+                                                renderValue={(selected) => (
+                                                    <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                                                        {selected.map((value) => (
+                                                            <Chip key={value.getID()} label={`${value.getVor_name()} ${value.getNach_name()}`} />
+                                                        ))}
+                                                    </Box>
+                                                )}
+                                            >
+
+                                                {allePersonen.map((person) => (
+                                                    <MenuItem
+                                                        key={person.getID()}
+                                                        value={person}
+                                                    >
+                                                        {person.getVor_name()} {person.getNach_name()}
+                                                    </MenuItem>
+                                                ))}
+                                            </Select>
+                                        </FormControl>
                                     </div>
                                     : null}
-                                <br /><br />
-                                {/* Auch hier wenn Aktivität ausgefüllt, dann neues Feld einfügen, um weitere Aktivitäten einzufügen
-                                Kapazität pro Aktivität eintragen!*/}
-                                <div>
-                                    {this.renderAktivitaetenBranches()}
-                                    <Button
-                                        sx={{
-                                            marginLeft: 2,
-                                            marginTop: 0.5,
-                                            width: 500,
-                                            height: 30,
-                                            alignItems: 'center',
-                                        }}
-                                        variant="contained"
-                                        className="addbranch"
-                                        onClick={this.appendAktivitaetenDiv}
-                                    >
-                                        <AddIcon />
-                                        &emsp;
-                                        <Typography>Weitere Aktivität hinzufügen</Typography>
-                                    </Button>
-                                </div>
+                                <br />
                             </DialogContentText>
                         </DialogContent>
                         <DialogActions>
                             <Button color='secondary' onClick={this.handleClose}>
                                 Abbrechen
                             </Button>
-                            <Button variant='contained' color='primary'>
+                            <Button variant='contained' color='primary' onClick={this.addProjekt}>
                                 Bestätigen
                             </Button>
                         </DialogActions>
