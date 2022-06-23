9307c5d0c97a54458ad6b68c28e210a7c8d81b43
Raphael-Mueller <73542589+Raphael-Mueller@users.noreply.github.com>
2022-06-23 15:08:02 +0200

    New Firebase api

--- reactfrontend/src/firebaseconfig.js
+++ reactfrontend/src/firebaseconfig.js
@@ -5,9 +5,13 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyBa-E8Q7ByWdPYcK04xat13Y2z-jc3vVZ4",
+  authDomain: "timetracker-72c63.firebaseapp.com",
+  projectId: "timetracker-72c63",
+  storageBucket: "timetracker-72c63.appspot.com",
+  messagingSenderId: "285158961633",
+  appId: "1:285158961633:web:97b66347931b3ebdd922a0"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export default firebaseConfig;
