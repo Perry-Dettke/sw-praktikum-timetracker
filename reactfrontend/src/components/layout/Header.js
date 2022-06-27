4d870fa2cad42cc16bf9bda9b666e77d818d039c
janadrawert <jd090@hdm-stuttgart.de>
2022-06-27 19:53:06 +0200

    Update Header.js

--- reactfrontend/src/components/layout/Header.js
+++ reactfrontend/src/components/layout/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Component } from 'react'
 import PropTypes from 'prop-types';
-import { Paper, Typography, Tabs, Tab } from '@mui/material';
+import { Grid, Paper, Typography, Tabs, Tab } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
-import { Box } from '@material-ui/core';
+
 import ProfileDropDown from '../dialogs/ProfileDropDown';
 
-/**
- * Shows the header with the main navigation Tabs within a Paper.
- */
+/*
+* Zeigt den Header mit der Navigationsleiste in einem Paper. 
+*/
+
 class Header extends Component {
 
   constructor(props) {
@@ -20,36 +21,45 @@ class Header extends Component {
     };
   }
 
-  /** Handles onChange events of the Tabs component */
+  /* Handles onChange events of the Tabs component */
   handleTabChange = (e, newIndex) => {
-    // console.log(newValue)
     this.setState({
       tabindex: newIndex
     })
   };
 
-  /** Renders the component */
+  /* Renders the component */
   render() {
     const { currentUser } = this.props;
     return (
-      <Paper variant='outlined' >
-        <ProfileDropDown currentUser={currentUser} />
-        <img src="logo.png" align='center' width="180" height="165"></img>
-      <div id="aussen">
-      <div class="b1">
-          <Typography variant='h3' component='h1' color='#323748' fontFamily='Verdana'>
-            TIME
-          </Typography>
-          </div>
-          <div class="b1">
-        <Typography variant='h3' component='h1'  color='#0098da' fontFamily='Courier'>
-          TRACKER
-        </Typography>
-        </div> 
-        </div>
-        <Typography variant='h5' component='h2' align='center' color='#323748' fontFamily='Verdana' >
-          Zeiterfassungssystem
-        </Typography>
+      <Paper variant='outlined'>
+        <Grid container justifyContent="center" spacing={0} alignItems="center" xs={12}>
+          <Grid item xs={2}/>
+          <Grid item xs={2}>
+            <img src="logo.png" align='center' width="180" height="165"></img>
+          </Grid>
+          <Grid item xs={2}>
+            <div id="aussen">
+              <div class="b1">
+                <Typography variant='h3' component='h1' color='#323748' fontFamily='Verdana'>
+                  TIME
+                </Typography>
+              </div>
+              <div class="b1">
+                <Typography variant='h3' component='h1'  color='#0098da' fontFamily='Courier'>
+                  TRACKER
+                </Typography>
+              </div> 
+            </div>
+            <Typography variant='h5' component='h2' align='center' color='#323748' fontFamily='Verdana' >
+              Zeiterfassungssystem
+            </Typography>
+          </Grid>
+          <Grid item xs={3}/>
+          <Grid item xs={1}>
+            <ProfileDropDown currentUser={currentUser} />
+          </Grid>
+        </Grid>
         {
             currentUser?
         
@@ -62,15 +72,11 @@ class Header extends Component {
           : null
         } 
       </Paper>
-    
-
-    )
-    
-  }
- 
+    )   
+  } 
 }
 
- 
+/**/ 
 Header.propTypes = {
 
   currentUser: PropTypes.object,
